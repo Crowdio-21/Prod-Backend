75b034cc5aad53348f015ad0ce9b9d1c0d7da088
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2026-01-27 19:26:46 +0530

    feat: include scheduler and checkpoint routes in API module

--- foreman/api/__init__.py
+++ foreman/api/__init__.py
@@ -4,6 +4,7 @@ API module for Foreman
 
 from . import routes
 from . import websockets
+from . import scheduler_routes
+from . import checkpoint_routes
 
-__all__ = ['routes', 'websockets']
-
+__all__ = ["routes", "websockets", "scheduler_routes", "checkpoint_routes"]
